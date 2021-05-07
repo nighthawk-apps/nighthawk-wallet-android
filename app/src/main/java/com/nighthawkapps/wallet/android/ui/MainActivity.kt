1925e86a22259a1ce1cf0dcda9116b27c3f9f9b2
Kevin Gorham <kevin.gorham@electriccoin.co>
2021-05-07 18:33:10 -0400

    New: Show errors that occur during startup.

--- app/src/main/java/com/nighthawkapps/wallet/android/ui/MainActivity.kt
+++ app/src/main/java/com/nighthawkapps/wallet/android/ui/MainActivity.kt
@@ -60,6 +60,7 @@ import com.nighthawkapps.wallet.android.di.component.MainActivitySubcomponent
 import com.nighthawkapps.wallet.android.di.component.SynchronizerSubcomponent
 import com.nighthawkapps.wallet.android.di.viewmodel.activityViewModel
 import com.nighthawkapps.wallet.android.ext.goneIf
+import com.nighthawkapps.wallet.android.ext.showCriticalError
 import com.nighthawkapps.wallet.android.ext.showCriticalProcessorError
 import com.nighthawkapps.wallet.android.ext.showScanFailure
 import com.nighthawkapps.wallet.android.ext.showUninitializedError
@@ -67,9 +68,11 @@ import com.nighthawkapps.wallet.android.ui.history.HistoryViewModel
 import com.nighthawkapps.wallet.android.ui.setup.WalletSetupViewModel
 import com.nighthawkapps.wallet.android.ui.util.INCLUDE_MEMO_PREFIXES_RECOGNIZED
 import com.nighthawkapps.wallet.android.ui.util.toUtf8Memo
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -210,18 +213,31 @@ class MainActivity : AppCompatActivity() {
             )
             twig("Synchronizer component created")
             synchronizerComponent.synchronizer().let { synchronizer ->
-                lifecycleScope.launch {
+                lifecycleScope.launch(CoroutineExceptionHandler(::onCriticalError)) {
+                    twig("starting synchronizer...")
                     synchronizer.onProcessorErrorHandler = ::onProcessorError
                     synchronizer.onChainErrorHandler = ::onChainError
                     walletSetupViewModel.onPrepareSync(synchronizer)
                     synchronizer.start(this)
                     mainViewModel.setSyncReady(true)
                     mainViewModel.setLoading(false)
+                    twig("...done starting synchronizer")
                 }
             }
         } else {
             twig("Ignoring request to start sync because sync has already been started!")
             mainViewModel.setLoading(false)
+        }
+    }
+
+    private fun onCriticalError(coroutineContext: CoroutineContext, error: Throwable) {
+        showCriticalError(
+            "Critical Error",
+            "An unrecognized error occurred:" +
+                    "\n\n${error.message}" +
+                    if (error.cause?.message != null) "\ncaused by: ${error.cause?.message}" else ""
+        ) {
+            throw error
         }
     }
 
