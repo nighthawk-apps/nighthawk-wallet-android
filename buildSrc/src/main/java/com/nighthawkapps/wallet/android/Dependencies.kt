8ed65597e59403acb966d6687799f914da4127db
Mandeep Bhalothia <42844945+Mandeepbhalothia@users.noreply.github.com>
2022-03-27 14:17:47 +0530

    Update to target and compile sdk version 32 and gradle 7.4.1

--- buildSrc/src/main/java/com/nighthawkapps/wallet/android/Dependencies.kt
+++ buildSrc/src/main/java/com/nighthawkapps/wallet/android/Dependencies.kt
@@ -5,10 +5,10 @@ object Deps {
     // `Deps.Kotlin.version` directly
     const val kotlinVersion = "1.6.10"
     const val navigationVersion = "2.4.0"
-    const val compileSdkVersion = 31
-    const val buildToolsVersion = "31.0.0"
+    const val compileSdkVersion = 32
+    const val buildToolsVersion = "32.0.0"
     const val minSdkVersion = 23
-    const val targetSdkVersion = 31
+    const val targetSdkVersion = 32
     const val versionName = "2.0.0"
     const val versionCode = 2_00_00_800 // last digits are alpha(0XX) beta(2XX) rc(4XX) release(8XX). Ex: 1_08_04_401 is an release candidate build of version 1.8.4 and 1_08_04_800 would be the final release.
     const val packageName = "com.nighthawkapps.wallet.android"
