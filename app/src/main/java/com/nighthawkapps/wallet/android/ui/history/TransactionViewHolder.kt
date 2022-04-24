b5b0017a8f7d1eee65d9a79e454731f70245fbb6
nighthawk24 <nighthawk24@users.noreply.github.com>
2022-04-23 21:23:46 -0400

    Fix strings.

--- app/src/main/java/com/nighthawkapps/wallet/android/ui/history/TransactionViewHolder.kt
+++ app/src/main/java/com/nighthawkapps/wallet/android/ui/history/TransactionViewHolder.kt
@@ -57,10 +57,10 @@ class TransactionViewHolder<T : ConfirmedTransaction>(itemView: View) : Recycler
                     when {
                         !toAddress.isNullOrEmpty() -> {
                             lineOne = if (isMined) str(R.string.ns_sent) else str(R.string.ns_sending)
-                            lineTwo = if (isMined) timestamp else str(R.string.transaction_status_pending)
+                            lineTwo = if (isMined) timestamp else str(R.string.ns_pending_confirmation)
                             // TODO: this logic works but is sloppy. Find a more robust solution to displaying information about expiration (such as expires in 1 block, etc). Then if it is way beyond expired, remove it entirely. Perhaps give the user a button for that (swipe to dismiss?)
                             if (!isMined && (expiryHeight != null) && (expiryHeight!! < mainActivity.latestHeight ?: -1)) lineTwo =
-                                str(R.string.transaction_status_expired)
+                                str(R.string.ns_expired)
                             amountDisplay = amountZec
                             if (memo.toUtf8Memo().isNotBlank()) {
                                 iconTransactionDrawable = R.drawable.ic_icon_memo
@@ -80,8 +80,8 @@ class TransactionViewHolder<T : ConfirmedTransaction>(itemView: View) : Recycler
                             arrowRotation = R.integer.transaction_arrow_rotation_received
                         }
                         else -> {
-                            lineOne = str(R.string.unknown)
-                            lineTwo = str(R.string.unknown)
+                            lineOne = str(R.string.ns_unknown)
+                            lineTwo = str(R.string.ns_unknown)
                             amountDisplay = amountZec
                             arrowRotation = R.integer.transaction_arrow_rotation_received
                         }
