f52e2c98aad045d3f6fdaf8f21e05c99934c855d
Mandeep Bhalothia <42844945+Mandeepbhalothia@users.noreply.github.com>
2022-04-30 11:40:55 +0530

    balance flip

--- app/src/main/java/com/nighthawkapps/wallet/android/ui/home/BalanceViewModel.kt
+++ app/src/main/java/com/nighthawkapps/wallet/android/ui/home/BalanceViewModel.kt
@@ -11,40 +11,47 @@ import com.nighthawkapps.wallet.android.ext.toAppString
 
 class BalanceViewModel : ViewModel() {
 
+    var isZecAmountState = true // To track user selected the zec balance mode or converted balance mode
+    var balanceAmountZec: String = ""
+
     fun getBalanceUIModel(
         sectionType: BalanceFragment.Companion.SectionType,
         homeUiModel: HomeViewModel.UiModel
     ): BalanceUIModel {
         return when (sectionType) {
             BalanceFragment.Companion.SectionType.SWIPE_LEFT_DIRECTION -> {
+                balanceAmountZec = ""
                 BalanceUIModel(
                     icon = ContextCompat.getDrawable(NighthawkWalletApp.instance.applicationContext, R.drawable.ic_icon_left_swipe),
-                    balanceAmount = "",
+                    balanceAmount = balanceAmountZec,
                     messageText = R.string.ns_swipe_left.toAppString(),
                     showIndicator = false
                 )
             }
             BalanceFragment.Companion.SectionType.TOTAL_BALANCE -> {
+                balanceAmountZec = (homeUiModel.saplingBalance.availableZatoshi + homeUiModel.transparentBalance.availableZatoshi).convertZatoshiToZecString()
                 BalanceUIModel(
                     icon = ContextCompat.getDrawable(NighthawkWalletApp.instance.applicationContext, R.drawable.ic_icon_total),
-                    balanceAmount = (homeUiModel.saplingBalance.availableZatoshi + homeUiModel.transparentBalance.availableZatoshi).convertZatoshiToZecString(),
+                    balanceAmount = balanceAmountZec,
                     expectingBalance = getExpectingBalance(homeUiModel.saplingBalance.totalZatoshi, homeUiModel.saplingBalance.availableZatoshi),
                     messageText = R.string.ns_total_balance.toAppString(),
                     showIndicator = true
                 )
             }
             BalanceFragment.Companion.SectionType.SHIELDED_BALANCE -> {
+                balanceAmountZec = homeUiModel.saplingBalance.availableZatoshi.convertZatoshiToZecString()
                 BalanceUIModel(
                     icon = ContextCompat.getDrawable(NighthawkWalletApp.instance.applicationContext, R.drawable.ic_icon_shielded),
-                    balanceAmount = homeUiModel.saplingBalance.availableZatoshi.convertZatoshiToZecString(),
+                    balanceAmount = balanceAmountZec,
                     messageText = R.string.ns_shielded_balance.toAppString(),
                     showIndicator = true
                 )
             }
             BalanceFragment.Companion.SectionType.TRANSPARENT_BALANCE -> {
+                balanceAmountZec = homeUiModel.transparentBalance.availableZatoshi.convertZatoshiToZecString()
                 BalanceUIModel(
                     icon = ContextCompat.getDrawable(NighthawkWalletApp.instance.applicationContext, R.drawable.ic_icon_transparent),
-                    balanceAmount = homeUiModel.transparentBalance.availableZatoshi.convertZatoshiToZecString(),
+                    balanceAmount = balanceAmountZec,
                     messageText = R.string.ns_transparent_balance.toAppString(),
                     showIndicator = true
                 )
